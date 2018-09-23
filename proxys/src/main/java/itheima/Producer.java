ab2568dec7dace7e65058b51759a222e89b556f8
Do <2714994098@qq.com>
2018-09-23 10:20:18 +0800

    代理工程第一次commit

---
+++ proxys/src/main/java/itheima/Producer.java
@@ -0,0 +1,15 @@
+package itheima;
+
+public interface Producer {
+    /**
+     * 销售
+     * @param money
+     */
+    public void saleProduct(float money);
+
+    /**
+     * 售后
+     * @param money
+     */
+    public void afterService(float money);
+}
